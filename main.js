a2451b4d006197bad3e75d759144240024e83de6
aliciacasiano <acasiano@gmail.com>
2017-02-21 20:39:52 -0500

    version 2
    sending to people to help me with wording!

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 function generateQuestions() {
   var questionsAndAnswers = [
-    { question: "What class is Aveline?",
-      answer: [ "warrior", "rogue", "darkspawn", "mage" ],
+    { question: "What class is Aveline Vallen, one of Hawke's companions in Dragon Age 2?",
+      answer: [ "Warrior", "Rogue", "Darkspawn", "Mage" ],
       rightAnswer: 0 },
-
-    { question: "In which Dragon Age novel did Cole appear before he became a companion in Dragon Age: Inquisition?",
-      answer: [ "<i>Last Flight</i>", "<i>The Stolen Throne</i>", "<i>The Calling</i>", "<i>Asunder</i>" ],
-      rightAnswer: 3 },
 
     { question: "What is the name of the guild of assassins that Zevran Aranai worked for in Dragon Age: Origins?",
       answer: [ "The Orlesian Fins", "The Ferelden Hounds", "The Antivan Crows", "The Free Marchers" ],
@@ -16,8 +12,12 @@ function generateQuestions() {
       answer: [ "Ostagar", "Jaundice", "Honnleath", "Denerim", "Gwaren" ],
       rightAnswer: 3 },
 
+    { question: "In which Dragon Age novel did Cole appear before he became a companion in Dragon Age: Inquisition?",
+      answer: [ "<i>Last Flight</i>", "<i>The Stolen Throne</i>", "<i>The Calling</i>", "<i>Asunder</i>" ],
+      rightAnswer: 3 },
+
     { question: "Put these royal Ferelden ranks in order from most to least powerful:",
-      answer: [ "arl, teyrn, bann", "teyrn, arl, bann", "teyrn, bann, arl", "bann, teyrn, arl" ],
+      answer: [ "Arl, teyrn, bann", "Teyrn, arl, bann", "Teyrn, bann, arl", "Bann, teyrn, arl" ],
       rightAnswer: 1 },
 
     { question: "What are Hawke's siblings' names?",
@@ -25,18 +25,18 @@ function generateQuestions() {
       rightAnswer: 1 },
 
     { question: "What is Bianca?",
-      answer: [ "a dwarven woman", "a crossbow", "a ship", "A and C", "A and B" ],
+      answer: [ "A dwarven woman", "A crossbow", "A ship", "A and C", "A and B" ],
       rightAnswer: 4 },
 
-    { question: "What term is used to denote mages who have had their magical abilities severed?",
-      answer: [ "abominations", "tranquil", "eluvian", "orzammars" ],
+    { question: "Which term is used to refer to mages who have had their magical abilities severed?",
+      answer: [ "Abomination", "Tranquil", "Eluvian", "Orzammar" ],
       rightAnswer: 1 },
 
     { question: "The magical ritual that recruits must undergo in order to become members of the legendary group of heroes known as the Grey Wardens is called:",
-      answer: [ "Val Royeaux", "the Harrowing", "Tal-Vashoth", "the Joining" ],
+      answer: [ "Val Royeaux", "The Harrowing", "Tal-Vashoth", "The Joining" ],
       rightAnswer: 3 },
 
-    { question: "Morrigan is the daughter of ___________, a powerful, ancient witch.",
+    { question: "Morrigan is the daughter of ___________, an ancient, powerful witch.",
       answer: [ "Merrill", "Flemeth", "Vivienne", "Solas" ],
       rightAnswer: 1 } ];
 
@@ -58,7 +58,7 @@ function initializeAnswers(numberOfQuestions) {
 
 function displayQuestion(currentQuestion, questionsAndAnswers) {
   // generating the question text
-  var labelText = (currentQuestion+1) + ' of 10';
+  var labelText = 'Question ' + (currentQuestion+1) + ' of 10';
   var mainText = questionsAndAnswers[currentQuestion].question;
   var answerText = '<form>';
 
@@ -110,7 +110,7 @@ function catchAnswer(currentQuestion, questionsAndAnswers, usersAnswers, usersCh
     // the answer is wrong
     $('.answerBlurb').addClass("wrong");
     currentAnswer = questionsAndAnswers[currentQuestion].answer[questionsAndAnswers[currentQuestion].rightAnswer];
-    $('.answerBlurb').html('You are incorrect! ' + currentAnswer + ' is the correct answer.');
+    $('.answerBlurb').html('You are incorrect!<br>' + currentAnswer + ' is the correct answer.');
     usersAnswers[currentQuestion] = "incorrect";
   }
   return usersAnswers;
@@ -219,16 +219,18 @@ function startTheQuiz () {
         $('.questionLabel').html('');
         $('h2').html('You answered ' + numberRight + ' out of 10 correctly!');
 
-        if ((numberRight >= 1) && (numberRight <= 3))
-          $('.checkboxChoices').html('1 to 3');
+	if (numberRight === 0)
+	  $('.checkboxChoices').html("Dragons? Ages? You don't know. Maybe if you give the quiz another go, you'll begin to understand.");
+        else if ((numberRight >= 1) && (numberRight <= 3))
+          $('.checkboxChoices').html("You're a really competent guesser!");
         else if ((numberRight >= 4) && (numberRight <= 6))
-          $('.checkboxChoices').html('4 to 6');
+          $('.checkboxChoices').html("An average showing! You certainly could have done worse, but you might do better if you...");
         else if ((numberRight === 7) || (numberRight ===8))
-          $('.checkboxChoices').html('7 or 8');
+          $('.checkboxChoices').html("Respectable! You know the world of Dragon Age better than most! There's no shame in stopping here, or you can show everyone what you <i>truly</i> know and...");
         else
-          $('.checkboxChoices').html('9 or 10');
+          $('.checkboxChoices').html("You are a Dragon Age master!<br><br>From Orlais to the Free Marches, the Tevinter Imperium to the Kokari Wilds, you know it all.");
 
-        $('.answerButton').html('Play Again!');
+        $('.answerButton').html('Try Again!');
 
         quizBegun = false;
         questionNumber = 0;
